d6a81d2cba2b42a15e40f005fa59c5baad9fb7cc
Michael Rooney <mrooney.github@rowk.com>
2012-07-01 15:55:43 -0400

    settings.py

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,8 @@ virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
 django-admin.py startproject $PROJECT website
+chmod +x website/manage.py
+
+grep SECRET_KEY website/$PROJECT/settings.py | cut -c 14-
+echo "move settings.py.template into place: put your secret key there and :%s/__PROJECT__/$PROJECT"
 
